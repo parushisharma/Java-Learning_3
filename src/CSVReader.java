bfcf7e525aeb4015078f49a20a229b7f72289d14
parushi <parushi@10.0.0.12>
2016-07-04 19:04:50 -0700

    add imports

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,33 +1,65 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class CSVReader {
+String[] countryNames;
+int[] yearLabels;
+double[][] cellularDatatables;
+Scanner scan;
 
-	public CSVReader(String fILENAME) {
-		// TODO Auto-generated constructor stub
-	}
+//throws FileNotFoundException
+public CSVReader(String filename) { 
+    try {
+    java.io.File file = new java.io.File(filename);
+    scan = new Scanner(file);
+    scan.nextLine();
+    String numLine = scan.nextLine();
+    final int n = Integer.parseInt(numLine.split(",")[1]); //Number is the string portion after the first comma
 
-	public static void main(String[] args) {
-		
+    //Allocate arrays with length n
+    countryNames = new String[n];
+    cellularDatatables = new double[n][];
 
-	}
+    //Read in the header line of years, parse and copy into yearNum
+    String[] yearHeaders = scan.nextLine().split(",");
+    final int m = yearHeaders.length-1;
+    yearLabels = new int[m];
+    for(int i=0;i<m;i++)
+    {
+    yearLabels[i] = Integer.parseInt(yearHeaders[i+1]); //i+1 to skip the first entry in the string arr
+    }
 
-	public String[] getCountryNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   //Now read until we run out of lines - put the first in country names and the rest in the table
+   int c = 0;
+   while(scan.hasNext())
+   {
+     String[] inputArr = scan.nextLine().split(",");
+     countryNames[c] = inputArr[0];
+     cellularDatatables[c] = new double[m];
+     for(int i = 0; i < m; i++)
+     {
+         cellularDatatables[c][i] = Double.parseDouble(inputArr[i+1]);
+       }
+    }
+        scan.close();
+    }
 
-	public int[] getYearLabels() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public double[][] getParsedTable() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int getNumberOfYears() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+    catch(FileNotFoundException e)
+    {
+        System.out.println(e.getMessage());
+    }
+}
+    public String[] getCountryNames(){
+        return countryNames;
+    }
+    public int[] getYearLabels(){
+        return yearLabels;
+    }
+    public double[][] getParsedTable(){
+        return cellularDatatables;
+    }
+    public int getNumberOfYears()
+    {
+        return yearLabels.length;
+    }
 }
