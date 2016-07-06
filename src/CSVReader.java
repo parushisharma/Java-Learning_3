98efb6623afb42b90547866c1019289e8b6bef38
parushi <parushi@10.0.0.12>
2016-07-05 17:33:42 -0700

    Add a while loop to fix null pointer exception

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -13,38 +13,36 @@ public CSVReader(String filename) {
     java.io.File file = new java.io.File(filename);
     Scanner scan = new Scanner(file);
     scan.nextLine();
-    String numLine = scan.nextLine();
-    final int n = Integer.parseInt(numLine.split(",")[1]); //Number is the string portion after the first comma
-    //Allocate arrays with length n
-    countryNames = new String[n];
-    cellularDatatables = new double[n][];
-
-    //Read in the header line of years, parse and copy into yearNum
-    String[] yearHeaders = scan.nextLine().split(",");
-    final int m = yearHeaders.length-1;
-    yearLabels = new int[m];
-    for(int i=0;i<m;i++)
-    {
-    this.yearLabels[i] = Integer.parseInt(yearHeaders[i+1]); //i+1 to skip the first entry in the string arr
+    while(scan.hasNextLine()) {
+    	String numLine = scan.nextLine();
+    	final int n = Integer.parseInt(numLine.split(",")[1]); //Number is the string portion after the first comma
+    	//Allocate arrays with length n
+    	countryNames = new String[n];
+    	cellularDatatables = new double[n][];
     }
+    
+    //Read in the header line of years, parse and copy into yearNum
+    	String[] yearHeaders = scan.nextLine().split(",");
+    	final int m = yearHeaders.length-1;
+    	yearLabels = new int[m];
+    		for(int i=0;i<m;i++) {
+    			this.yearLabels[i] = Integer.parseInt(yearHeaders[i+1]); //i+1 to skip the first entry in the string arr
+    		}
 
    //Now read until we run out of lines - put the first in country names and the rest in the table
-   int c = 0;
-   while(scan.hasNext())
-   {
-     String[] inputArr = scan.nextLine().split(",");
-     countryNames[c] = inputArr[0];
-     cellularDatatables[c] = new double[m];
-     for(int i = 0; i < m; i++)
-     {
-         cellularDatatables[c][i] = Double.parseDouble(inputArr[i+1]);
-       }
-    }
-        scan.close();
+    		int c = 0;
+    		while(scan.hasNext()) {
+    			String[] inputArr = scan.nextLine().split(",");
+    			countryNames[c] = inputArr[0];
+    			cellularDatatables[c] = new double[m];
+    			for(int i = 0; i < m; i++) {
+    				cellularDatatables[c][i] = Double.parseDouble(inputArr[i+1]);
+    			}
+    		}
+    		scan.close();
     }
 
-    catch(FileNotFoundException e)
-    {
+    catch(FileNotFoundException e) {
         System.out.println(e.getMessage());
     }
 }
